37a298e92ed6a10fe7782954e5e7a072e2f60618
Ali Hassan Raza <Ahraza.DevOps@gmail.com>
2023-08-30 15:58:30 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TrainerPageComponent } from './pages/trainer-page/trainer-page.component';
+import { PokeCatalogPageComponent } from './pages/poke-catalog-page/poke-catalog-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    TrainerPageComponent,
+    PokeCatalogPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/poke-catalog-page/poke-catalog-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-catalog-page',
+  templateUrl: './poke-catalog-page.component.html',
+  styleUrls: ['./poke-catalog-page.component.css']
+})
+export class PokeCatalogPageComponent {
+
+}

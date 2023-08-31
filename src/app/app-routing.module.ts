7ae8f7de44a9824218cd01ae19fb91516fdb9bb2
Pau Go Si <paugosi@hotmail.com>
2023-08-31 21:20:34 +0200

    Implement Guard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPage } from './pages/landing-page/landing-page.page';
 import { PokeCatalogPage } from './pages/poke-catalog-page/poke-catalog-page.page';
 import { TrainerPage } from './pages/trainer-page/trainer-page.page';
+import { AuthGuard } from './guards/auth.guard';
+
 
 const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/login"
+  },
   {
     path: 'login',
     component: LandingPage
   },
   {
     path: 'trainer',
-    component: TrainerPage
+    component: TrainerPage,
+    canActivate: [AuthGuard]
   },
   {
     path: 'poke-catalog',
-    component: PokeCatalogPage
+    component: PokeCatalogPage,
+    canActivate: [AuthGuard]
   },
   {
     path: '',

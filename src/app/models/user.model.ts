ad9a2b092ce4ef29a024643187c7e57d3b744afd
Pau Go Si <paugosi@hotmail.com>
2023-09-01 22:45:01 +0200

    Implement displaying name and images of pokemons bassed on the user logged in

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,15 @@
 import { Pokemon } from "./pokemon.model";
-
+/*
 export interface User {
     id: number;
     username: string;
     pokemon: Pokemon[];
+}*/
+
+
+export interface User {
+    id: number;
+    username: string;
+    pokemon: string[]; // This should be an array of strings, as per the given API data.
 }
+
--- src/app/pages/trainer-page/trainer-page.page.ts
+++ src/app/pages/trainer-page/trainer-page.page.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-trainer-page',
   templateUrl: './trainer-page.page.html',
   styleUrls: ['./trainer-page.page.css']
 })
-export class TrainerPage {
+export class TrainerPage implements OnInit {
+  public userPokemon: any[] = [];
 
+  constructor(
+    private userService: UserService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    const user = this.userService.user;
+    if(user && user.pokemon) {
+      user.pokemon.forEach((pokemonName: string) => {
+        this.http.get<any>(`${environment.pokeApiUrl}${pokemonName}`)
+        .subscribe(pokemonData => {
+          console.log(pokemonData);
+          //const id = this.extractIdFromUrl(pokemonData.url);
+          const id = pokemonData.id;
+
+          //const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+          const imageUrl = pokemonData.sprites.front_default;
+
+          this.userPokemon.push({
+            name: pokemonName,
+            imageUrl: imageUrl
+          });
+        });
+      });
+    }
+  }
+/*
+  private extractIdFromUrl(url: string): number {
+    const match = url.match(/(\d+)\/$/);
+    return match ? +match[1] : 0;
+  }*/
 }

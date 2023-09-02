f8264bc53ff44c628ed74ddd482a317d5d9dd552
Pau Go Si <paugosi@hotmail.com>
2023-09-02 19:18:50 +0200

    Fix unsync frontend list order with Angular local state

--- src/app/pages/trainer-page/trainer-page.page.ts
+++ src/app/pages/trainer-page/trainer-page.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { forkJoin } from 'rxjs';
+import { ChangeDetectorRef } from '@angular/core';
 
 // This page is for the trainer's Pokémon collection
 @Component({
@@ -17,59 +19,32 @@ export class TrainerPage implements OnInit {
   // This constructor injects the UserService into this class
   constructor(
     private userService: UserService,
-    private http: HttpClient
+    private http: HttpClient,
+    private cdRef: ChangeDetectorRef
   ) {}
 
-  // This method is called when the page is loaded
   ngOnInit(): void {
-    // Get the user's Pokémon collection from the UserService.
     const user = this.userService.user;
-    // If the user has a Pokémon collection, then add each Pokémon's name to the userPokemon array.
-    if(user && user.pokemon) {
-      user.pokemon.forEach((pokemonName: string) => {
-        this.http.get<any>(`${environment.pokeApiUrl}${pokemonName}`)
-        // This is a callback function that will be called when the HTTP request completes
-        .subscribe(pokemonData => {
-          // Add the Pokémon's name and image to the userPokemon array.
-          // This is the data that will be displayed in the Pokémon collection.
-          // The data is in the format:
-          // {
-          //   name: <"Pokeman name">,
-          //   imageUrl: "https://xxxxxxxxxxxxxxxxx/${id}.png"
-          // }
-          // The name and image are extracted from the pokemonData object.
-          // The name is the Pokémon's name, and the image is its image URL.
-          // The image URL is obtained from the pokemonData object.
-          // The image URL is the URL of the image that is displayed in the Pokémon collection.
-          console.log(pokemonData);
-          const id = pokemonData.id;
-          const imageUrl = pokemonData.sprites.front_default;
-          this.userPokemon.push({
-            name: pokemonName,
-            imageUrl: imageUrl
-          });
+
+    if (user && user.pokemon) {
+        // Prepare an array of Observables for each Pokémon's details
+        const observables = user.pokemon.map((pokemonName: string) => {
+            return this.http.get<any>(`${environment.pokeApiUrl}${pokemonName}`);
         });
-      });
+
+        // Use forkJoin to execute all observables
+        forkJoin(observables).subscribe(pokemonDataArray => {
+            this.userPokemon = pokemonDataArray.map((pokemonData, index) => {
+                return {
+                    name: user.pokemon[index],
+                    imageUrl: pokemonData.sprites.front_default
+                };
+            });
+        });
+        this.cdRef.detectChanges();
     }
   }
-  /*
-  // This method is called when the user clicks the "Remove" button
-  removePokemon(index: number): void {
-    const user = this.userService.user;
-
-    if (user) {
-      // Remove the Pokémon from the user's collection.
-      user.pokemon.splice(index, 1);
-
-      // Now update this user in the backend (i.e., save the updated user data).
-      // This can be done using a service call to update the user in the database.
-      // Here's a hypothetical updateUserService - we'd have to implement this:
-      this.userService.updateUser(user).subscribe(() => {
-        // Refresh the displayed Pokémon list.
-        this.userPokemon.splice(index, 1);
-      });
-    }
-  }*/
+  
   removePokemon(index: number): void {
     const user = this.userService.user;
   

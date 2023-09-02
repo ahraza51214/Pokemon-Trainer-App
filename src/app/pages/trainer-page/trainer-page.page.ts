6c4457cdffac778e78efd6f6d2841948671bf1c7
Pau Go Si <paugosi@hotmail.com>
2023-09-02 19:59:00 +0200

    Delete unused file and make comments to the codes

--- src/app/pages/trainer-page/trainer-page.page.ts
+++ src/app/pages/trainer-page/trainer-page.page.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+
+// forkJoin is used to execute multiple Observables at once
 import { forkJoin } from 'rxjs';
+
+// ChangeDetectorRef is used to detect changes in the frontend
 import { ChangeDetectorRef } from '@angular/core';
+
 
 // This page is for the trainer's Pokémon collection
 @Component({
@@ -22,17 +27,23 @@ export class TrainerPage implements OnInit {
     private http: HttpClient,
     private cdRef: ChangeDetectorRef
   ) {}
-
+  // This method is called when the page is loaded
   ngOnInit(): void {
     const user = this.userService.user;
 
+    // If the user is logged in, then load their Pokémon
     if (user && user.pokemon) {
+
         // Prepare an array of Observables for each Pokémon's details
         const observables = user.pokemon.map((pokemonName: string) => {
             return this.http.get<any>(`${environment.pokeApiUrl}${pokemonName}`);
         });
 
         // Use forkJoin to execute all observables
+        // and then assign the results to the userPokemon array
+        // This is done in parallel, so the userPokemon array
+        // will contain the results in the same order as the
+        // Pokémon names in the user's Pokémon list
         forkJoin(observables).subscribe(pokemonDataArray => {
             this.userPokemon = pokemonDataArray.map((pokemonData, index) => {
                 return {

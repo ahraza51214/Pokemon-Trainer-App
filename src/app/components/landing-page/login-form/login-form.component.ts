dc4221be7b21c957722f1575a31c6cc2e57c8753
Pau Go Si <paugosi@hotmail.com>
2023-09-04 01:06:36 +0200

    More things to added

--- src/app/components/landing-page/login-form/login-form.component.ts
+++ src/app/components/landing-page/login-form/login-form.component.ts
@@ -1,6 +1,5 @@
 import { Component,Output,EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { LoginService } from 'src/app/services/login.service';
 import { UserService } from 'src/app/services/user.service';
